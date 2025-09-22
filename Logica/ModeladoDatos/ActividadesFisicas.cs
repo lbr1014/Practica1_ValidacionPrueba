74d02d7428e31cc74a3b272303bb1ca0cdb8a462
lbr1014 <lbr1014@alu.ubu.es>
2025-09-22 13:58:50 +0200

    Atributos ActividadesFisicas

--- Logica/ModeladoDatos/ActividadesFisicas.cs
+++ Logica/ModeladoDatos/ActividadesFisicas.cs
@@ -8,5 +8,17 @@ namespace Logica.ModeladoDatos
 {
     internal class ActividadesFisicas
     {
+        private string idActividadFisica;
+        private string nombreActividadFisica;
+        private int duracion;
+        private string descripcion;
+
+        public ActividadesFisicas(string idActividadFisica, string nombreActividadFisica, int duracion, string descripcion)
+        {
+            this.idActividadFisica = idActividadFisica;
+            this.nombreActividadFisica = nombreActividadFisica;
+            this.duracion = duracion;
+            this.descripcion = descripcion;
+        }
     }
 }
