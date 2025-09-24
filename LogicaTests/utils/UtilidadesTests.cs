4993862375f6891a02533fc2f22ff3796f3d36e3
lbr1014 <lbr1014@alu.ubu.es>
2025-09-24 16:54:29 +0200

    Test utilidades

---
+++ LogicaTests/utils/UtilidadesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica1.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades.Tests
+{
+    [TestClass()]
+    public class UtilidadesTests
+    {
+        [TestMethod()]
+        public void EncriptarContraseñaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
