e13dac3068535b96f7e779b8975afcf53d20018f
lbr1014 <lbr1014@alu.ubu.es>
2025-09-24 18:08:02 +0200

    Interfaz

---
+++ Datos/CapaDatos.cs
@@ -0,0 +1,54 @@
+﻿using Logica.ModeladoDatos;
+using Practica1.ModeladoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class CapaDatos : ICapaDatos
+    {
+        List<Usuario> idUsuarios = new List<Usuario>();
+        public bool GuardaActividad(ActividadesFisicas e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GuardaUsuario(Usuario u)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActividadesFisicas LeeActividad(int idElemento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario LeeUsuario(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumActividades(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumUsuariosActivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidaUsuario(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Datos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Datos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Datos")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("9ab058df-58f4-44be-9197-c8a7c2665145")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
