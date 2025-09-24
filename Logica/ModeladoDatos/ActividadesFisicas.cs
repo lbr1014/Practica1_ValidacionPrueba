bac7447b9a9425509d8dcad557ba06b2e0f72041
lbr1014 <lbr1014@alu.ubu.es>
2025-09-24 14:19:14 +0200

    Test para ActividadesFisicas

--- Logica/ModeladoDatos/ActividadesFisicas.cs
+++ Logica/ModeladoDatos/ActividadesFisicas.cs
@@ -26,5 +26,29 @@ namespace Logica.ModeladoDatos
 
         public String Descripcion { get { return this.descripcion; } set { this.descripcion = value; } }
 
+        public override bool Equals(object obj)
+        {
+            return obj is ActividadesFisicas fisicas &&
+                   idActividadFisica == fisicas.idActividadFisica &&
+                   nombreActividadFisica == fisicas.nombreActividadFisica &&
+                   duracion == fisicas.duracion &&
+                   descripcion == fisicas.descripcion &&
+                   NombreActividad == fisicas.NombreActividad &&
+                   Duracion == fisicas.Duracion &&
+                   Descripcion == fisicas.Descripcion;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1261025783;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(idActividadFisica);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(nombreActividadFisica);
+            hashCode = hashCode * -1521134295 + duracion.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(descripcion);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NombreActividad);
+            hashCode = hashCode * -1521134295 + Duracion.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Descripcion);
+            return hashCode;
+        }
     }
 }
--- LogicaTests/ModeladoDatos/ActividadesFisicasTests.cs
+++ LogicaTests/ModeladoDatos/ActividadesFisicasTests.cs
@@ -24,9 +24,45 @@ namespace Practica1.ModeladoDatos.Tests
             var actividad = CrearActividadCorrecta("AF-001", "Correr", 30, "Correr en el gimnasio");
 
             Assert.AreEqual("Correr", actividad.NombreActividad);
+            actividad.NombreActividad = "Nadar";
+            Assert.AreEqual("Nadar", actividad.NombreActividad);
+
             Assert.AreEqual(30, actividad.Duracion);
+            actividad.Duracion = 40;
+            Assert.AreEqual(40, actividad.Duracion);
+
             Assert.AreEqual("Correr en el gimnasio", actividad.Descripcion);
+            actividad.Descripcion = "Hacer largos en una piscina olimpica";
+            Assert.AreEqual("Hacer largos en una piscina olimpica", actividad.Descripcion);
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            var actividad1 = CrearActividadCorrecta("AF-001", "Correr", 30, "Correr en el gimnasio");
+            var actividad2 = CrearActividadCorrecta("AF-001", "Correr", 30, "Correr en el gimnasio");
+            Assert.IsTrue(actividad1.Equals(actividad2));
+
+            var actividad3 = CrearActividadCorrecta("AF-002", "Nadar", 40, "Hacer largos en una piscina olimpica");
+            Assert.IsFalse(actividad1.Equals(actividad3));
+
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            var actividad1 = CrearActividadCorrecta("AF-001", "Correr", 30, "Correr en el gimnasio");
+            var actividad2 = CrearActividadCorrecta("AF-001", "Correr", 30, "Correr en el gimnasio");
+
+            int hash1 = actividad1.GetHashCode();
+            int hash2 = actividad2.GetHashCode();
+            Assert.AreEqual(hash1, hash2);
+
+            var actividad3 = CrearActividadCorrecta("AF-002", "Nadar", 40, "Hacer largos en una piscina olimpica");
+            int hash3 = actividad3.GetHashCode();
+            Assert.AreNotEqual(hash1, hash3);
         }
 
     }
+
 }
